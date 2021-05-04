a53e579d9540e2209beb399f8e78f48533136360
Adam Li <a.li@amd.com>
2021-05-04 23:50:12 +0000

    Some cleaning up

--- modeling.py
+++ modeling.py
@@ -958,29 +958,29 @@ def layer_norm(input_tensor, name=None):
     return tf.keras.layers.LayerNormalization(axis=-1, epsilon=1e-12)(inputs=input_tensor)
 
 
-def get_assignment_map_from_checkpoint(tvars, init_checkpoint):
-    """Compute the union of the current variables and checkpoint variables."""
-    assignment_map = {}
-    initialized_variable_names = {}
+# def get_assignment_map_from_checkpoint(tvars, init_checkpoint):
+#     """Compute the union of the current variables and checkpoint variables."""
+#     assignment_map = {}
+#     initialized_variable_names = {}
 
-    name_to_variable = collections.OrderedDict()
-    for var in tvars:
-        name = var.name
-        m = re.match("^(.*):\\d+$", name)
-        if m is not None:
-            name = m.group(1)
-        name_to_variable[name] = var
+#     name_to_variable = collections.OrderedDict()
+#     for var in tvars:
+#         name = var.name
+#         m = re.match("^(.*):\\d+$", name)
+#         if m is not None:
+#             name = m.group(1)
+#         name_to_variable[name] = var
 
-    init_vars = tf.train.list_variables(init_checkpoint)
+#     init_vars = tf.train.list_variables(init_checkpoint)
 
-    assignment_map = collections.OrderedDict()
-    for x in init_vars:
-        (name, var) = (x[0], x[1])
-        if name not in name_to_variable:
-            continue
-        #assignment_map[name] = name
-        assignment_map[name] = name_to_variable[name]
-        initialized_variable_names[name] = 1
-        initialized_variable_names[name + ":0"] = 1
+#     assignment_map = collections.OrderedDict()
+#     for x in init_vars:
+#         (name, var) = (x[0], x[1])
+#         if name not in name_to_variable:
+#             continue
+#         #assignment_map[name] = name
+#         assignment_map[name] = name_to_variable[name]
+#         initialized_variable_names[name] = 1
+#         initialized_variable_names[name + ":0"] = 1
 
-    return (assignment_map, initialized_variable_names)
+#     return (assignment_map, initialized_variable_names)
--- run_pretraining_notpu.py
+++ run_pretraining_notpu.py
@@ -162,16 +162,6 @@ def model_fn_builder(bert_config, learning_rate,
     masked_lm_weights = features["masked_lm_weights"]
     next_sentence_labels = features["next_sentence_labels"]
 
-    is_training = (mode == tf.estimator.ModeKeys.TRAIN)
-
-    # model = modeling.BertModel(
-    #     config=bert_config,
-    #     is_training=is_training,
-    #     input_ids=input_ids,
-    #     input_mask=input_mask,
-    #     token_type_ids=segment_ids,
-    #     #use_one_hot_embeddings=use_one_hot_embeddings
-    #     )
     model = modeling.BertModel(config=bert_config)
     model_output = model([input_ids, input_mask, segment_ids])
 
@@ -190,27 +180,10 @@ def model_fn_builder(bert_config, learning_rate,
     next_sentence_loss = tf.identity(next_sentence_loss, name='nsp_loss')
     total_loss = tf.identity(total_loss, name='total_loss')
 
-    # TODO: Convert model loading to TF2
     tvars = tf.compat.v1.trainable_variables()
-
-    initialized_variable_names = {}
-    # #if init_checkpoint and (hvd == None or hvd.rank() == 0):
-    # if init_checkpoint:
-    #   #logging.info("**** Init Checkpoint {} {} ****".format(hvd.rank(), init_checkpoint))
-    #   logging.info("**** Init Checkpoint {} ****".format(init_checkpoint))
-    #   (assignment_map, initialized_variable_names) = \
-    #     modeling.get_assignment_map_from_checkpoint(tvars, init_checkpoint)
-    #   tf.compat.v1.train.init_from_checkpoint(init_checkpoint, assignment_map)
-    #   # checkpoint = tf.train.Checkpoint(root=model)
-    #   # checkpoint.restore(tf.train.latest_checkpoint(init_checkpoint))
-    #   #model.load_weights(tf.train.latest_checkpoint(init_checkpoint))
-    #   #print(tf.train.list_variables(init_checkpoint))
-
     logging.info("**** Trainable Variables ****")
     for var in tvars:
       init_string = ""
-      if var.name in initialized_variable_names:
-        init_string = ", *INIT_FROM_CKPT*"
       logging.info("  name = %s, shape = %s%s", var.name, var.shape,
                       init_string)
 
@@ -237,14 +210,8 @@ def model_fn_builder(bert_config, learning_rate,
         masked_lm_example_loss = tf.reshape(masked_lm_example_loss, [-1])
         masked_lm_ids = tf.reshape(masked_lm_ids, [-1])
         masked_lm_weights = tf.reshape(masked_lm_weights, [-1])
-        # masked_lm_accuracy = tf.compat.v1.metrics.accuracy(
-        #     labels=masked_lm_ids,
-        #     predictions=masked_lm_predictions,
-        #     weights=masked_lm_weights)
         masked_lm_accuracy = tf.keras.metrics.Accuracy()
         masked_lm_accuracy.update_state(masked_lm_ids, masked_lm_predictions, masked_lm_weights)
-        # masked_lm_mean_loss = tf.compat.v1.metrics.mean(
-        #     values=masked_lm_example_loss, weights=masked_lm_weights)
         masked_lm_mean_loss = tf.keras.metrics.Mean()
         masked_lm_mean_loss.update_state(masked_lm_example_loss, masked_lm_weights)
 
@@ -253,12 +220,8 @@ def model_fn_builder(bert_config, learning_rate,
         next_sentence_predictions = tf.argmax(
             input=next_sentence_log_probs, axis=-1, output_type=tf.int32)
         next_sentence_labels = tf.reshape(next_sentence_labels, [-1])
-        # next_sentence_accuracy = tf.compat.v1.metrics.accuracy(
-        #     labels=next_sentence_labels, predictions=next_sentence_predictions)
         next_sentence_accuracy = tf.keras.metrics.Accuracy()
         next_sentence_accuracy.update_state(next_sentence_labels, next_sentence_predictions)
-        # next_sentence_mean_loss = tf.compat.v1.metrics.mean(
-        #     values=next_sentence_example_loss)
         next_sentence_mean_loss = tf.keras.metrics.Mean()
         next_sentence_mean_loss.update_state(next_sentence_example_loss)
 
@@ -288,24 +251,13 @@ def model_fn_builder(bert_config, learning_rate,
 
   return model_fn
 
-# TODO: Change this lm part to Keras layers
+
 def get_masked_lm_output(bert_config, input_tensor, output_weights, positions,
                          label_ids, label_weights):
   """Get loss and log probs for the masked LM."""
   input_tensor = gather_indexes(input_tensor, positions)
 
-  #with tf.compat.v1.variable_scope("cls/predictions"):
   with tf.name_scope("cls/masked_lm_pred") as scope:
-    # We apply one more non-linear transformation before the output layer.
-    # This matrix is not used after pre-training.
-    # with tf.compat.v1.variable_scope("transform"):
-    #   input_tensor = tf.compat.v1.layers.dense(
-    #       input_tensor,
-    #       units=bert_config.hidden_size,
-    #       activation=modeling.get_activation(bert_config.hidden_act),
-    #       kernel_initializer=modeling.create_initializer(
-    #           bert_config.initializer_range))
-    #   input_tensor = modeling.layer_norm(input_tensor)
     im_pred_dense = tf.keras.layers.Dense(
       units=bert_config.hidden_size,
       activation=modeling.get_activation(bert_config.hidden_act),
@@ -314,12 +266,6 @@ def get_masked_lm_output(bert_config, input_tensor, output_weights, positions,
     input_tensor = im_pred_dense(input_tensor)
     input_tensor = modeling.layer_norm(input_tensor)
 
-    # The output weights are the same as the input embeddings, but there is
-    # an output-only bias for each token.
-    # output_bias = tf.compat.v1.get_variable(
-    #     "output_bias",
-    #     shape=[bert_config.vocab_size],
-    #     initializer=tf.compat.v1.zeros_initializer())
     output_bias = tf.Variable(
       name="output_bias",
       shape=[bert_config.vocab_size],
@@ -352,18 +298,7 @@ def get_next_sentence_output(bert_config, input_tensor, labels):
 
   # Simple binary classification. Note that 0 is "next sentence" and 1 is
   # "random sentence". This weight matrix is not used after pre-training.
-  #with tf.compat.v1.variable_scope("cls/seq_relationship"):
   with tf.name_scope("cls/next_sentense_pred") as scope:
-    # output_weights = tf.compat.v1.get_variable(
-    #     "output_weights",
-    #     shape=[2, bert_config.hidden_size],
-    #     initializer=modeling.create_initializer(bert_config.initializer_range))
-    # output_bias = tf.compat.v1.get_variable(
-    #     "output_bias", shape=[2], initializer=tf.compat.v1.zeros_initializer())
-
-    # logits = tf.matmul(input_tensor, output_weights, transpose_b=True)
-    # logits = tf.nn.bias_add(logits, output_bias)
-
     seq_rel_dense = tf.keras.layers.Dense(
       units=2,
       kernel_initializer=modeling.create_initializer(bert_config.initializer_range),
@@ -503,24 +438,14 @@ def main(_):
 
   model_fn = model_fn_builder(
       bert_config=bert_config,
-      #init_checkpoint=FLAGS.init_checkpoint,
-      #init_checkpoint=None,
       learning_rate=FLAGS.learning_rate,
       num_train_steps=FLAGS.num_train_steps,
       num_warmup_steps=FLAGS.num_warmup_steps)
-      # use_tpu=False,
-      # use_one_hot_embeddings=False,
-      # use_hvd=False,
-      # use_amp=False)
 
   params = {
     'batch_size': FLAGS.train_batch_size
   }
 
-  # estimator = tf.compat.v1.estimator.Estimator(
-  #     model_fn=model_fn,
-  #     config=run_config,
-  #     params=params)
   estimator = tf.estimator.Estimator(
       model_fn=model_fn,
       config=run_config,
